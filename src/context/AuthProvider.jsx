0db3b11ffe9719c9ff8966296524163dabcad5d1
Sanchita Bajpai <159428199+SanchitaBajpai02@users.noreply.github.com>
2025-07-05 23:02:27 +0530

    Add files via upload

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,25 @@
+import React, { createContext, useEffect, useState } from 'react'
+import { getLocalStorage, setLocalStorage } from '../utils/localStorage'
+
+export const AuthContext = createContext()
+
+const AuthProvider = ({children}) => {
+// localStorage.clear()
+    const [userData, setUserData]=useState(null)
+    // const data = getLocalStorage()
+    // // console.log(data.employees)
+    useEffect(()=>{
+        setLocalStorage()
+        const {employees} =getLocalStorage()
+        setUserData(employees)
+    },[])
+  return (
+    <div>
+        <AuthContext.Provider value={[userData, setUserData]}>
+            {children}
+        </AuthContext.Provider>
+    </div>
+  )
+}
+
+export default AuthProvider
